c4b4d87c6a2cf1fb99e932c778d31fd98010da44
zhouyao <897180859@qq.com>
2018-05-09 12:31:34 +0800

    简单爬虫：百度百科1000条词汇

---
+++ html_outputer.py
@@ -0,0 +1,32 @@
+# 网络输出器
+
+class HtmlOutputer(object):
+
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        fout = open('output.html', 'w', encoding='utf-8')
+
+        fout.write('<html>')
+        fout.write('<body>')
+        fout.write('<head><meta charset="utf-8"></head>')  # 告诉浏览器使用何种编码
+        fout.write('<table>')
+
+        for data in self.datas:
+            fout.write('<tr>')
+            fout.write('<td>%s</td>' % data['url'])
+            fout.write('<td>%s</td>' % data['title'])
+            fout.write('<td>%s</td>' % data['summary'])
+            fout.write('</tr>')
+
+        fout.write('</table>')
+        fout.write('</body>')
+        fout.write('</html>')
+
+        fout.close()
